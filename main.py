45c8977c3a66729e736e615657c6859dac063118
oskkorz <oskkorz@gmail.com>
2024-04-08 21:48:13 +0200

    finished, dont ask how

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+book_path = "books/frankenstein.txt" 
+
 def main():
-    book_path = "books/frankenstein.txt"
+    #book_path = "books/frankenstein.txt"
     text = read_function(book_path)
     #count_words = len(text.split())
     num_words = count_words(text)
     letters_count = char_occurences(text)
-    print(num_words, letters_count)
+
+    print (f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print("")
+
+    letters_list = []
+    for char in letters_count:
+        letters_list.append((char, letters_count[char]))
+    letters_list.sort(reverse=True, key=sort_on)
+    
+    for char, count in letters_list:
+        print (f"The '{char}' char was found: {count} times")
+
+    print("--- End report ---")
+
+   
 
 def read_function(path):
     with open(path) as f:
@@ -19,14 +36,16 @@ def char_occurences(text):
     low = text.lower()
     #return len(low)
     for char in low:
-        if char in char_count:
-            char_count[char] += 1
+        if char.isalpha():
+            if char in char_count:
+                char_count[char] += 1
 
-        else:
-            char_count[char] = 1
-
+            else:
+                char_count[char] = 1
     return char_count
 
+def sort_on(dict):
+    return dict[1]
 
 
 main()
